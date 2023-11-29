8e596e0390546f022f1a7026068a27e4335c756e
redarmykk <red.kirill.army@gmail.com>
2023-11-30 02:59:59 +0600

    бургер хуево сделан, лого в nav сидит сори

--- index.js
+++ index.js
@@ -23,3 +23,27 @@ menuLinks.forEach(function(el){
 
   }) 
 })
+
+
+$(document).ready(function(){
+
+  $('.feedback__left').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
+
